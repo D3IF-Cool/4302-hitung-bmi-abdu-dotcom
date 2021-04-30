d0c3a217aac447af262a1a775de89afac56744b8
Muhammad_Abduh_Husaini_Batubara <abduhbatubara@student.telkomuniversity.ac.id>
2021-04-30 15:03:00 +0700

    3.2.Memperbaiki navigasi dengan view model

--- app/src/main/java/org/d3if3067/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if3067/hitungbmi/ui/hitung/HitungFragment.kt
@@ -1,4 +1,4 @@
-package org.d3if3067.hitungbmi.ui
+package org.d3if3067.hitungbmi.ui.hitung
 
 import android.content.Intent
 import android.os.Bundle
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if3067.hitungbmi.R
 import org.d3if3067.hitungbmi.data.KategoriBmi
 import org.d3if3067.hitungbmi.databinding.FragmentHitungBinding
@@ -18,7 +19,7 @@ class HitungFragment : Fragment() {
     private val viewModel: HitungViewModel by viewModels()
 
     private lateinit var binding : FragmentHitungBinding
-    private lateinit var kategoriBmi : KategoriBmi
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHitungBinding.inflate(layoutInflater,container,false)
@@ -28,15 +29,8 @@ class HitungFragment : Fragment() {
         binding.buttonReset.setOnClickListener{
             resetBmi()
         }
+        binding.saranButton.setOnClickListener{ viewModel.mulaiNavigasi()}
         binding.shareButton.setOnClickListener { shareData() }
-
-        binding.saranButton.setOnClickListener{
-            view : View ->
-            view.findNavController().navigate(
-                HitungFragmentDirections.
-                        actionHitungFragmentToSaranFragment(kategoriBmi)
-            )
-        }
         return binding.root
     }
 
@@ -48,6 +42,13 @@ class HitungFragment : Fragment() {
             binding.kategoriTextView.text = getString(R.string.kategori_x,
                 getKategori(it.kategori))
             binding.buttonGroup.visibility = View.VISIBLE
+        })
+
+        viewModel.getNavigasi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            findNavController().navigate(HitungFragmentDirections
+                .actionHitungFragmentToSaranFragment(it))
+            viewModel.selesaiNavigasi()
         })
     }
 
