807576f421decaa010137ca03ed2fd7b2b3905f6
Muhammad_Abduh_Husaini_Batubara <abduhbatubara@student.telkomuniversity.ac.id>
2021-04-30 15:52:40 +0700

    3.4.Menyimpan data ke database

---
+++ app/src/main/java/org/d3if3067/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -0,0 +1,67 @@
+package org.d3if3067.hitungbmi.ui.hitung
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.d3if3067.hitungbmi.data.HasilBmi
+import org.d3if3067.hitungbmi.data.KategoriBmi
+import org.d3if3067.hitungbmi.db.BmiDao
+import org.d3if3067.hitungbmi.db.BmiEntity
+
+class HitungViewModel(private val db: BmiDao) : ViewModel() {
+
+    // Hasil BMI bisa null jika pengguna belum menghitung BMI
+    private val hasilBmi = MutableLiveData<HasilBmi?>()
+    // navigasi akan bernilai null ketika tidak bernavigasi
+    private val navigasi = MutableLiveData<KategoriBmi?>()
+
+    // Variabel ini sudah berupa LiveData (tidak mutable),
+    // sehingga tidak perlu dijadikan private
+    val data = db.getLastBmi()
+
+    fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
+        val tinggiCm = tinggi.toFloat() / 100
+        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
+        val kategori = if (isMale) {
+            when {
+                bmi < 20.5 -> KategoriBmi.KURUS
+                bmi >= 27.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        }
+        else {
+            when {
+                bmi < 18.5 -> KategoriBmi.KURUS
+                bmi >= 25.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        }
+        hasilBmi.value = HasilBmi(bmi, kategori)
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                val dataBmi = BmiEntity(
+                    berat = berat.toFloat(),
+                    tinggi = tinggi.toFloat(),
+                    isMale = isMale
+                    )
+                db.insert(dataBmi)
+            }
+        }
+    }
+
+    fun mulaiNavigasi(){
+        navigasi.value = hasilBmi.value?.kategori
+    }
+    fun selesaiNavigasi(){
+        navigasi.value = null
+    }
+    fun getHasilBmi() : LiveData<HasilBmi?> = hasilBmi
+    fun getNavigasi() : LiveData<KategoriBmi?> = navigasi
+
+
+}
